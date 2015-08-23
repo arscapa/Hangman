db8ece56e8fd37f007f5ea9ab746ec80e87bc1ee
Tony Scapardine <arscapa@gmail.com>
2015-08-23 15:18:22 -0500

    Completed basic game functionality. Need to iron out bugs (i.e. --> the display for the letters you have guessed correctly doesn't work correctly when there is more than one of the same letter in the word you are guessing)

--- ps2_hangman.py
+++ ps2_hangman.py
@@ -70,33 +70,56 @@ def hangman():
     while guess_left != 0 and guessed_word != answer:
 
         print "I am thinking of a word that is %d letters long" %(len(answer))
-        print answer
-        print displayed_word
+        print guessed_word
         print "You have ", guess_left, " guesses left"
         print "Letters you have guessed: ", guessed_letters
-    
+        print answer    
 
-        letter_guess = input("Guess a letter   ")
+        letter_guess = raw_input("Guess a letter   ").lower()
+
+        if letter_guess in guessed_letters:
+            print "You've already guessed that letter, duh, now I'm deducting 3 guesses "
+            guess_left  -= 3 
+            
+        elif letter_guess in answer:
+            print "Dang, well you got this one"
+            letter_position = answer.find(letter_guess)
+
+
+            ##Convert current guessed letters to a list in order to change value of letter you guessed from "-" to guessed letter
+            your_guess = list(guessed_word)
+
+            ##Changes dash to guessed letter
+            your_guess[letter_position] = letter_guess
+
+            guessed_letters.append(letter_guess)
+            guessed_word = "".join(your_guess)
+
+        else:
+            print "Sorry that letter is not in the word I'm thinking of you fool"
+            guessed_letters.append(letter_guess)
+            guess_left -=1
 
 
 
+    if guess_left == 0:
+        print "Sorry but you really suck at this game and ran out of guesses. The word was " \
+        + answer
 
-##        
-##
-##    
-##    guess_left += -1
-##    print guessed_letters
-##    print guess_left
-##
-##
-##if guess_left == 0:
-##    print "Sorry but you really suck at this game and ran out of guesses. The word was " \
-##    + answer
+    if guessed_word == answer:
+        print "Congrats, you guessed the word I was thinking of, what do you want a medal or something, get lost?"
+
 
 hangman()
 
 
+replay = raw_input("Play again? (Y/N) ")
 
+if replay.lower() == "y" or replay.lower() == "yes":
+    hangman()
+
+else:
+    print "Scared?"
 
 
 
